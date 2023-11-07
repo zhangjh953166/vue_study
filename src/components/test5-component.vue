0807d2f6ba430775820938e4e4b34f695c9b7ca0
zhangjh953166 <58389214@qq.com>
2023-11-07 21:59:01 +0800

    循环的控制

--- src/components/test5-component.vue
+++ src/components/test5-component.vue
@@ -1,13 +1,25 @@
 <script setup>
     import { ref } from 'vue'
+    // ref and function
     const count = ref(0)
     function increment(){
         count.value++
     }
+    // v-if
+    const awesome = ref(true)
+    // v-for
+    const parentMessage = ref('Parent')
+    const items = ref([{ message: 'Foo' }, { message: 'Bar' }])
 </script>
 
 <template>
     <button @click="increment">
         {{ count }}
     </button>
+    <button @click="awesome = !awesome">Toggle</button>
+    <h1 v-if="awesome">Vue is awesome!</h1>
+    <h1 v-else>Oh no 😢</h1>
+    <li v-for="(item, index) in items">
+        {{ parentMessage }} - {{ index }} - {{ item.message }}
+    </li>
 </template>
