dd975bfe2f402151edadba725be156394cc5c483
zhangjh953166 <58389214@qq.com>
2023-11-15 19:43:46 +0800

    学习到侦听器

--- src/components/test5-component.vue
+++ src/components/test5-component.vue
@@ -1,5 +1,6 @@
 <script setup>
     import { ref } from 'vue'
+    import { watch } from 'vue'
     // ref and function
     const count = ref(0)
     function increment(){
@@ -10,16 +11,57 @@
     // v-for
     const parentMessage = ref('Parent')
     const items = ref([{ message: 'Foo' }, { message: 'Bar' }])
+    //select
+    const selected = ref('A')
+
+    const options = ref([
+    { text: 'One', value: 'A' },
+    { text: 'Two', value: 'B' },
+    { text: 'Three', value: 'C' }
+    ])
+
+    //watch
+    const question = ref('')
+    const answer = ref('Questions usually contain a question mark. ;-)')
+
+    watch(question,async(newQuestion,oldQuestion)=>{
+        if (newQuestion.indexOf('?')>-1) {
+            answer.value = 'Thinking...'
+            try{
+                const res = await fetch('https://yesno.wtf/api')
+                answer.value = (await res.json()).answer
+            } catch (error) {
+                answer.value = 'Error! Could not reach the API. ' + error
+            }
+        }
+    })
 </script>
 
 <template>
+<p>
     <button @click="increment">
         {{ count }}
     </button>
+</p>
     <button @click="awesome = !awesome">Toggle</button>
+<p>
     <h1 v-if="awesome">Vue is awesome!</h1>
     <h1 v-else>Oh no 😢</h1>
+</p>
     <li v-for="(item, index) in items">
         {{ parentMessage }} - {{ index }} - {{ item.message }}
     </li>
+<p>
+    <select v-model="selected">
+        <option v-for="option in options" :value="option.value">
+            {{ option.text }}
+        </option>
+    </select>
+    <div>Selected: {{ selected }}</div>
+</p>
+<p>
+    Ask a yes/no question:
+    <input v-model="question" />
+</p>
+<p>{{ answer }}</p>
 </template>
