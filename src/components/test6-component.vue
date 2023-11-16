0a612570910b13eb56bb04596efb40f5b61e4972
zhangjh953166 <58389214@qq.com>
2023-11-16 16:24:45 +0800

    模板引用

---
+++ src/components/test6-component.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref,onMounted } from 'vue'
+const input = ref(null)
+const list = ref([
+    'a','b','c'
+])
+const itemRefs = ref([])
+onMounted(() => {
+    input.value.focus()
+    console.log('a',itemRefs.value)
+})
+
+</script>
+<template>
+<p>
+    <input ref="input" />
+</p>
+<ul>
+    <li v-for="item in list" ref="itemRefs">
+      {{ item }}
+    </li>
+</ul>
+</template>
