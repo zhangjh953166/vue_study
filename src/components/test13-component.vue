2148598bc52fe30f54314a534d43f33cea81e64e
zhangjh953166 <58389214@qq.com>
2023-11-29 19:32:41 +0800

    学习到v-model

---
+++ src/components/test13-component.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue'
+import CustomInput from './CustomInput.vue'
+  
+const message = ref('hello')
+</script>
+
+<template>
+  <CustomInput v-model="message" /> {{ message }}
+</template>
